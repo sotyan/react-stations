9c5a1e82163c23bf2ae8513bd9eb11ce8b5602ae
sotyan <happy.tanbarin2@gmail.com>
2024-09-16 21:01:58 +0900

    Station5,6complete

--- src/App.jsx
+++ src/App.jsx
@@ -9,17 +9,17 @@ export const App = () => {
   const [dogUrl, setDogUrl] = useState (
     'https://images.dog.ceo/breeds/spaniel-brittany/n02101388_6057.jpg',
   )
-  // const changeDogImage = () => {
-  //   fetch('https://dog.ceo/api/breeds/image/random')
-  //   .then(res => res.json())
-  //   .then(json => setDogUrl(json.message));
-  // }
+  const changeDogImage = () => {
+    fetch('https://dog.ceo/api/breeds/image/random')
+    .then(res => res.json())
+    .then(json => setDogUrl(json.message));
+  }
   return (
     <div>
       <header>Dogアプリ</header>
       <p>犬の画像を表示するサイトです。</p>
-      <img src={dogUrl} alt="犬の画像"/>
-      {/* <button onClick={changeDogImage}>チェンジ</button> */}
+      <img src={dogUrl} alt="犬の画像"/><br />
+      <button onClick={changeDogImage}>更新</button>
     </div>
   )
 }
