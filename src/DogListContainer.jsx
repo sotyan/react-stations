813aa04145e8e6d40eddb5aff91ce974df64e270
sotyan <happy.tanbarin2@gmail.com>
2024-09-24 18:07:15 +0900

    犬種データ取り出し成功

--- src/DogListContainer.jsx
+++ src/DogListContainer.jsx
@@ -3,30 +3,34 @@ import { useState, useEffect } from "react"
 import BreedsSelect from "./BreedsSelect";
 
 export const DogListContainer = () => {
-  const [breeds, setBreeds] = useState ([]);
+  const [breeds, setBreeds] = useState ([""]);
   const [selectedBreed, setSelectedBreed] = useState(["Select"]);
 
 
   useEffect(() => {
     fetch('https://dog.ceo/api/breeds/list/all')
     .then(res => res.json())
-    .then(json => setBreeds(json.message))
-    // .then(data => {
-    //   const list = data.message
-    //   setBreeds(Object.keys(list))
-    // })
-    console.log("a");
-
+    .then(json => setBreeds(Object.keys(json.message)))
+    // console.log(breeds)
+    // console.log(breeds.length)
   },[]
   );
-  // const breed = breeds.map((breed) => <option>breed</option>);
-  // const option = breeds.map((message)=><option value={message}>{message}</option>)
+
+
+  const optionItems = breeds.map((breed) =>
+  <option value={breed} key={breed}>{breed}</option>
+  );
+  
+
+
+
   return(
   <>
   <div className="breedsList">
     <label htmlFor="breedsList">BreedsList</label>
-    <select name="" id="breedsList">
-      <option value="apple">apple</option>
+    <select name="breedsList" id="breedsList">
+      {optionItems}
+      
     </select>
   </div>
   {/* <BreedsSelect breeds={breeds} /> */}
